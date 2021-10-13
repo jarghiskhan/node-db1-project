46630ca68c6e94310cf860b78c46145c97d49460
jarghiskhan <jared.l.wyatt@outlook.com>
2021-10-13 16:57:55 -0400

    some tests passing

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,13 +1,24 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // DO YOUR MAGIC
+ return db('accounts')
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts')
+  .where({id})
+  .first();
 }
 
 const create = account => {
   // DO YOUR MAGIC
+  return db('accounts')
+  .insert(account)
+  .then(ids =>{
+    return getById(ids[0])
+  })
 }
 
 const updateById = (id, account) => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,37 @@
 const router = require('express').Router()
-
+const Accounts = require('./accounts-model')
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  console.log("get running")
+  Accounts.getAll()
+  .then((account)=>{
+    res.status(200).json(account)
+  })
 })
 
 router.get('/:id', (req, res, next) => {
+  const {id}= req.params
   // DO YOUR MAGIC
+  Accounts.getById(id)
+  .then((account)=>{
+    res.status(200).json(account)
+  })
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+// router.post('/', (req, res, next) => {
+//   // DO YOUR MAGIC
+// })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-});
+// router.put('/:id', (req, res, next) => {
+//   // DO YOUR MAGIC
+// });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+// router.delete('/:id', (req, res, next) => {
+//   // DO YOUR MAGIC
+// })
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+// router.use((err, req, res, next) => { // eslint-disable-line
+//   // DO YOUR MAGIC
+// })
 
 module.exports = router;
